5d76a7ec0e53c1ddfd8186a52790d1fd72480619
Ansh Agarwal <agarwalansh0511@gmail.com>
2024-11-18 01:00:01 +0530

    updated routes folder

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
+require("dotenv").config({ path: "./config/config.env" });
 const express = require("express");
-require("dotenv").config({path:"/config/config.env"});
-const mongoose = require("mongoose");
-const cors = require("cors");
-const morgan=require("morgan");
-const connectDB=require("./config/db");
-const auth=require("./middlewares/auth");
+const morgan = require("morgan");
+
+const connectDB = require("./config/db");
+
+const auth = require("./middlewares/auth");
+
 const app = express();
-app.use(cors());
+
+// middlewares
 app.use(express.json());
 app.use(morgan("tiny"));
-app.get("/protected", auth, (req, res)=>{
-    return res.status(200).json({...req.user._doc});
+app.use(require("cors")());
+
+// routes
+app.use("/api", require("./routes/auth"));
+app.use("/api", require("./routes/contact"));
+
+// server configurations.
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`server listening on port: ${PORT}`);
 });
-
-app.use("/api/", require("./routes/auth"));
-
-
-const PORT=process.env.PORT || 8000;
-
-app.listen(PORT,async () =>{
-    try{
-        await connectDB();
-        console.log(`Server running on port: ${PORT}`);
-    }
-    // await connectDB();
-    catch(err){
-        console.log(err);
-    }
-} );
---
+++ models/Contact.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const { type } = require("server/reply");
+
+const ContactSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: [true, "name is required."],
+  },
+  last_name: {
+    type: String,
+    required: [true, "lastname is required."],
+  },
+  email: {
+    type: String,
+    required: [true, "email is required."],
+  },
+  phone: {
+    type: Number,
+    required: [true, "phone number is required."],
+  },
+  company: {
+    type: String,
+    required:[true, "company name required."],
+  },
+  job_title:{
+    type: String,
+    required:[true, "what is your job title?"],
+  },
+  postedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const Contact = new mongoose.model("Contact", ContactSchema);
+
+const validateContact = (data) => {
+  const schema = Joi.object({
+    // name: Joi.string().min(4).max(50).required(),
+    // address: Joi.string().min(4).max(100).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.number().min(7).max(10000000000).required(),
+  });
+
+  return schema.validate(data);
+};
+
+module.exports = {
+  validateContact,
+  Contact,
+};
